1983e1d2231ed9ca1416e5beb76fa8d2a0b87779
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-08-24 19:47:26 +0900

    【機能追加】時間、スコア追加

--- tsumutsumu/Assets/Scripts/GameScene/CreateBall.cs
+++ tsumutsumu/Assets/Scripts/GameScene/CreateBall.cs
@@ -2,22 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CreateBall : MonoBehaviour {
 
 	//オブジェクト参照
 	public GameObject ballPrefab; //ボールプレハブ
+	public GameObject countDown; //カウントダウンタイマーとなるオブジェクト
+	public GameObject timer; //制限時間タイマーとなるオブジェクト
+	public GameObject score; //スコア表示
 	public Sprite[] ballSprite = new Sprite[5]; //ボールの画像
 
 	//メンバ変数
 	private GameObject firstBall; //最初にタッチしたボール
-	private List<GameObject> removableBallList; //消去するボールのリスト
 	private GameObject lastBall; //直前にドラッグしたボール
+	private List<GameObject> removableBallList; //消去するボールのリスト
 	private string currentName; //現在リストにあるボールの名前
+	private bool isPlaying = false; //プレイ中かどうか
+	private Text timerText; //タイマーのテキスト
+	private Text countDownText; //カウントダウンのテキスト
+	private Text scoreText; //スコアテキスト
+	private float timeLimit = 60; //制限時間
+	private int countTime = 5; //カウントダウンの秒数
+	private int currentScore = 0; //現在のスコア
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine ("DropBall", 55);  
+		countDownText = countDown.GetComponent<Text>(); //タイマーを取得
+		timerText = timer.GetComponent<Text>(); //制限時間タイマー
+		scoreText = score.GetComponent<Text>(); //scoreTextを設定
+		StartCoroutine ("CountDown"); //カウントダウン開始
+	}
+
+	private IEnumerator CountDown(){
+		int count = countTime;
+		while(count > 0){
+			countDownText.text = count.ToString (); //カウントダウンのテキスト
+			yield return new WaitForSeconds(1); //1秒待つ
+			count -= 1; //カウントを１つ減らす
+		}
+		countDownText.text = "Start!";
+		isPlaying = true;
+		yield return new WaitForSeconds (1);
+		countDown.SetActive (false);
+		StartCoroutine ("DropBall", 55);  //ボールを降らす
+		StartCoroutine ("StartTimer"); //制限時間のカウントを開始
+	}
+
+	private IEnumerator StartTimer(){
+		float count = timeLimit;
+		while (count > 0) {
+			timerText.text = count.ToString ();
+			yield return new WaitForSeconds (1);
+			count -= 1;
+		}
+		countDown.SetActive (true);
+		countDownText.text = "Finish";
+		OnDragEnd ();
+		isPlaying = false;
 	}
 
 	//count個ボールを降らせる
@@ -44,16 +86,20 @@ public class CreateBall : MonoBehaviour {
 	}
 
 	private void Update(){
-		if (Input.GetMouseButtonDown (0) && firstBall == null) {
-			//ボールをドラッグし始めた時
-			OnDragStart ();
-		} else if (Input.GetMouseButtonUp (0)) {
-			//ボールをドラッグし終わった時
-			OnDragEnd ();
-		} else if (firstBall != null) {
-			//ボールをドラッグしている途中
-			OnDragging();
+		if (isPlaying) {
+			if (Input.GetMouseButtonDown (0) && firstBall == null) {
+				//ボールをドラッグし始めた時
+				OnDragStart ();
+			} else if (Input.GetMouseButtonUp (0)) {
+				//ボールをドラッグし終わった時
+				OnDragEnd ();
+			} else if (firstBall != null) {
+				//ボールをドラッグしている途中
+				OnDragging ();
+			}
 		}
+
+		scoreText.text = currentScore.ToString(); //現在のスコアを表示
 	}
 
 	private void OnDragStart(){
@@ -80,6 +126,9 @@ public class CreateBall : MonoBehaviour {
 				for (int i = 0; i < length; i++) {
 					Destroy (removableBallList [i]); //リストにあるボールを消去
 				}
+
+				currentScore += (CalculateBaseScore(length) + 50 * length);
+
 				StartCoroutine ("DropBall", length); //消した分だけボールを生成
 			} else {
 				//消去するリストに3個以上ボールがない時
@@ -129,5 +178,14 @@ public class CreateBall : MonoBehaviour {
 		Color originalColor = obj.GetComponent<SpriteRenderer> ().color;
 		obj.GetComponent<SpriteRenderer> ().color = new Color (originalColor.r, originalColor.g, originalColor.b, transparency);
 	
+	}
+
+	private int CalculateBaseScore(int num){
+		int tempScore = 50 * (num) * (num + 1) - 300;
+		return tempScore;
+	}
+
+	public void Reset(){
+		SceneManager.LoadScene ("GameScene");
 	}
 }
